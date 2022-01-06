86157106261f0f703b5ca7ac2b12c5c42a6902c1
Surobhi <surobhi1395@gmail.com>
2022-01-06 20:44:38 +0530

    Initial Commit

---
+++ src/main/java/com/sample/springdockerex/SpringDockerExApplication.java
@@ -0,0 +1,13 @@
+package com.sample.springdockerex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDockerExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDockerExApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sample/springdockerex/SpringDockerExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.springdockerex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDockerExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
